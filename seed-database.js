9439594385ec49b465def1a44c91f958db69e6c4
Your Name <your-email@example.com>
2025-10-20 13:33:29 +0000

    configured the database

--- seed-database.js
+++ seed-database.js
@@ -6,7 +6,7 @@ const connectionPool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
 });
